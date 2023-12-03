bcd5106cba00e55bea3a27f4e3767084c3851582
arvajkhan <arvajkhan91@gmail.com>
2023-12-03 13:17:47 +0530

    add all

---
+++ src/Context/TodoContext.js
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [{ id: 1, todo: "todo message", completed: false }],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export function useTodo() {
+  return useContext(TodoContext);
+}
